11673ec8ecaf55a418ab8be67e2d7b409cdf91c8
Lee Robinson <robinsonleeuk@gmail.com>
2021-02-16 09:46:37 +0000

    Converts all pdfs

--- run_from_here.py
+++ run_from_here.py
@@ -1,6 +1,6 @@
 from wordscraper import *
 
-project_name = "Test Project"
+project_name = "UNGA and G20"
 
 project_dir, pdf_dir, txt_dir, results_dir = directories(project_name)
 
@@ -41,15 +41,15 @@ stop_words = [
 ]
 
 
-# create_txt_files(input_dir=pdf_dir, output_dir=txt_dir, results_dir=results_dir)
+create_txt_files(input_dir=pdf_dir, output_dir=txt_dir, results_dir=results_dir)
 
-write_doc(
-    input_dir=txt_dir,
-    results_dir=results_dir,
-    searchwords=searchwords,
-    stop_words=stop_words,
-    project_name=project_name,
-)
+# write_doc(
+#     input_dir=txt_dir,
+#     results_dir=results_dir,
+#     searchwords=searchwords,
+#     stop_words=stop_words,
+#     project_name=project_name,
+# )
 
 # entries_in_dir(
 #     input_dir=txt_dir,
