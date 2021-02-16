d1eef7b19ec1fb4f5c477768f580450ea4aa80b1
Lee Robinson <robinsonleeuk@gmail.com>
2021-02-16 14:27:42 +0000

    Adds spellchecker to modules

--- wordscraper.py
+++ wordscraper.py
@@ -13,8 +13,17 @@ import pandas as pd
 from docx import Document
 from docx.text.paragraph import Paragraph
 from openpyxl import load_workbook
-from openpyxl.styles import (Alignment, Border, Color, Font, GradientFill,
-                             NamedStyle, PatternFill, Side, colors)
+from openpyxl.styles import (
+    Alignment,
+    Border,
+    Color,
+    Font,
+    GradientFill,
+    NamedStyle,
+    PatternFill,
+    Side,
+    colors,
+)
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.workbook import Workbook
 from pdfminer.converter import PDFPageAggregator
@@ -24,6 +33,7 @@ from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
 from pdfminer.pdfparser import PDFParser
+from spellchecker import SpellChecker
 
 from modules.convertpdfs import *
 from modules.directories import *
